b9207538eadc59c7b777c567544cf3b51986f980
Bryce Davis <bad1128@gmail.com>
2014-04-11 10:44:49 -0400

    Add basic REPL that repeats input

--- lib/emerald.rb
+++ lib/emerald.rb
@@ -1,5 +1,19 @@
 require "emerald/version"
 
 module Emerald
-  # Your code goes here...
+    class EmeraldREPL
+        def initialize
+            repl = -> prompt do
+                print prompt
+                handle_input(gets.chomp!)
+            end
+            loop do
+                repl[">> "]
+            end
+        end
+
+        def handle_input(input)
+            puts(" => #{input}")
+        end
+    end
 end
